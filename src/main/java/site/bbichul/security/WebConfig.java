c05e66d7b1b092977c40c73a802e87f3b6e3dbad
김성훈 <shkim11997@gmail.com>
2021-12-06 16:48:48 +0900

    벡엔드배포 30

---
+++ src/main/java/site/bbichul/security/WebConfig.java
@@ -0,0 +1,17 @@
+package site.bbichul.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342", "https://qwqwca.shop")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
