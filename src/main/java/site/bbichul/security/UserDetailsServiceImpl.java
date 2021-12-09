665723079e57e5e46e1d9ecfbad6127bdc72a1b7
김성훈 <shkim11997@gmail.com>
2021-12-09 15:00:35 +0900

    백엔드배포max max

--- src/main/java/site/bbichul/security/UserDetailsServiceImpl.java
+++ src/main/java/site/bbichul/security/UserDetailsServiceImpl.java
@@ -17,11 +17,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 로그인 닉네임 있는 확인
+        // 아이디 존재 유무 판별
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Can't find " + username));
-        // status 까지 판별
-        User user1 = userRepository.findByUsernameAndStatus(username, false).orElseThrow(
+
+        // 회원상태가 탈퇴인지 판별
+        User user1 = userRepository.findByUsernameAndStatus(username, true).orElseThrow(
                 () -> new UsernameNotFoundException("아이다가 탈퇴 상태 입니다")
         );
 
